a9b2d85116c64bef04ab98012d6b6ff2069d0a65
ayacandoit <ayasalah77777@gmail.com>
2025-02-28 12:21:43 +0200

    development2

--- app/src/main/java/com/example/foodplanner/HomeScreen/View/View/Home_Activity.java
+++ app/src/main/java/com/example/foodplanner/HomeScreen/View/View/Home_Activity.java
@@ -42,6 +42,8 @@ public class Home_Activity extends AppCompatActivity implements HomeBridge.View 
 
     private List<Recipe> recipeList = new ArrayList<>();
     //
+    //
+    ///
     private List<Category> categoryList = new ArrayList<>();
     private List<Area> areaList = new ArrayList<>();
 
